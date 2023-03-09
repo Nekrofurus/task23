7fce469ea57a6e17b4369dea2b959779e61cff66
VVCephei <vladgol90@gmail.com>
2023-03-09 15:02:40 +0200

    первая рабочая версия

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите количество элементов таблици кубов");
+int number = Convert.ToInt32(Console.ReadLine());
+var i = 1;
+    for(i = 1; i < number; i++)
+    {
+        int x = i;
+        int cubeTabl = x * x * x;
+        Console.Write($"{cubeTabl} ,");
+    }
+
